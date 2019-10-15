b1a158959dc2f3fa7aec11ff616d107327130f4a
Aaron Rock <aaronrk@mail.com>
2019-10-15 09:27:28 -0400

    controller setup

--- BlogMvc/Controllers/HomeController.cs
+++ BlogMvc/Controllers/HomeController.cs
@@ -10,34 +10,19 @@ namespace BlogMvc.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        
+        public ViewResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+        
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
+        public ViewResult Contact()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
+
---
+++ BlogMvc/Controllers/PostController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogMvc.Controllers
+{
+    public class PostController : Controller
+    {
+        public ViewResult Index(int ID, string Title, string Body, string Image, string Author)
+        {
+            var model = new PostModel();
+            model.ID = 32;
+            model.Title = "Best Blog Ever";
+            model.Body = "I have never read such engaging material";
+            model.Image = "Some Cool Image";
+            model.Author = "Joe Smith";
+
+            return View(model);
+
+        }
+
+        public ViewResult Details(int ID, string Title, string Body, string Image, string Author)
+        {
+            var model = new PostModel();
+            model.ID = 32;
+            model.Title = "Best Blog Ever";
+            model.Body = "I have never read such engaging material";
+            model.Image = "Some Cool Image";
+            model.Author = "Joe Smith";
+
+            return View(model);
+        }
+
+    }
+}
