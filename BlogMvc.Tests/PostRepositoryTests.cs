864848574548a2bc7d0a66d21c0500cec95eba47
Aaron Rock <aaronrk@mail.com>
2019-10-17 16:07:55 -0400

    added categories

---
+++ BlogMvc.Tests/PostRepositoryTests.cs
@@ -0,0 +1,82 @@
+﻿using BlogMvc.Data;
+using BlogMvc.Models;
+using BlogMvc.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace BlogMvc.Tests
+{
+    public class PostRepsositoryTests : IDisposable
+    {
+
+        private DataContext db;
+        private PostRepository underTest;
+
+        public PostRepsositoryTests()
+        {
+            db = new DataContext();
+            db.Database.BeginTransaction();
+
+            underTest = new PostRepository(db);
+        }
+
+        public void Dispose()
+        {
+            db.Database.RollbackTransaction();
+        }
+
+        [Fact]
+        public void Count_Starts_At_Four()
+        {
+            var count = underTest.Count();
+
+            Assert.Equal(4, count);
+        }
+
+        [Fact]
+        public void Create_Increases_Count()
+        {
+            underTest.Create(new PostModel() { Title = "This Is Great" });
+
+            var count = underTest.Count();
+            Assert.Equal(5, count);
+        }
+
+        [Fact]
+        public void GetById_Returns_Created_Item()
+        {
+            var expectedHeroModel = new PostModel() { Title = "This Is Better" };
+            underTest.Create(expectedHeroModel);
+
+            var result = underTest.GetByID(expectedHeroModel.ID);
+
+            //Assert.Equal(expectedHeroModel.Name, result/*.Name*/);
+        }
+
+        [Fact]
+        public void Delete_Reduces_Count()
+        {
+            var HeroModel = new PostModel() { Title = "This Is Awesome" };
+            underTest.Create(HeroModel);
+
+            underTest.Delete(HeroModel);
+            var count = underTest.Count();
+
+            Assert.Equal(4, count);
+        }
+
+        [Fact]
+        public void GetAll_Returns_All()
+        {
+            underTest.Create(new PostModel() { Title = "This Is Not Good" });
+            underTest.Create(new PostModel() { Title = "This Is Really Good" });
+
+            var all = underTest.GetAll();
+
+            Assert.Equal(6, all.Count());
+        }
+    }
+}
--- BlogMvc/Controllers/PostController.cs
+++ BlogMvc/Controllers/PostController.cs
@@ -6,15 +6,22 @@ using BlogMvc.Models;
 using BlogMvc.Repository;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace BlogMvc.Controllers
 {
     public class PostController : Controller
     {
         IRepository<PostModel> postrepo;
 
+        public object PostModelID { get; private set; }
+
         public PostController(IRepository<PostModel>postrepo)
         { this.postrepo = postrepo; }
-    
+
+
+        
+
+
         public ViewResult Index()
         {
             var model = postrepo.GetAll();
@@ -31,6 +38,49 @@ namespace BlogMvc.Controllers
             var model = postrepo.GetByID(id);
             return View(model);
         }
+
+        [HttpPost]
+        public ActionResult Create(PostModel post)
+        {
+            postrepo.Create(post);
+            return RedirectToAction("Details");
+        }
+
+        [HttpGet]
+        public ViewResult CreateByPostID(int id)
+        {
+            ViewBag.PostModellID = id;
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            var model = postrepo.GetByID(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(PostModel post)
+        {
+            postrepo.Delete(post);
+            return RedirectToAction("Details");
+        }
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            var model = postrepo.GetByID(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PostModel post)
+        {
+            postrepo.Edit(post);
+            return RedirectToAction("Details");
+        }
+
+
 
     }
 }
