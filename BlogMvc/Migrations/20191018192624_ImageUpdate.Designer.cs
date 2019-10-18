dd70020f000e0378ed8cf2ce44e2add7f9dab73b
Bryan Kautzman <bdkautz@icloud.com>
2019-10-18 15:30:15 -0400

    Updated Seed Data

--- BlogMvc/Migrations/20191018192624_ImageUpdate.Designer.cs
+++ BlogMvc/Migrations/20191018192624_ImageUpdate.Designer.cs
@@ -4,14 +4,16 @@ using BlogMvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogMvc.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191018192624_ImageUpdate")]
+    partial class ImageUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
